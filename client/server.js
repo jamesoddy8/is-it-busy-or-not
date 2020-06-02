536a37e4e0ec5672358aaa1c47b44aac483ebb6b
Peter Stevens <Peterstevens510@gmail.com>
2020-06-02 12:19:45 +0100

    got twitter API Working

--- client/server.js
+++ client/server.js
@@ -13,3 +13,22 @@ app.get('/', function (req, res) {
 });
 
 app.listen(process.env.PORT || 8080);
+
+
+var Twitter = require('twitter');
+
+var client = new Twitter({
+  consumer_key: 
+  consumer_secret:
+  bearer_token: 
+})
+
+function makeApiCall() { 
+  client.get('search/tweets', {q: 'node.js'}, function(error, tweets, response) {
+    console.log(tweets);
+ });
+}
+
+console.log("----")
+makeApiCall()
+console.log("----")
--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,40 @@
-import React from 'react';
+import React,{Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+var Twitter = require('twitter');
 
-function App() {
-  return (
+  var client = new Twitter({
+    // consumer_key: '',
+    // consumer_secret: '',
+    bearer_token: process.env.TWITTER_BEARER_TOKEN
+  })
+
+
+  class App extends Component {
+
+    constructor(props){
+      super(props);
+
+    }
+
+  makeApiCall() {
+    client.get('search/tweets', {q: 'node.js'}, function(error, tweets, response) {
+      console.log(tweets);
+   });
+  }
+
+
+  render() {
+    return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <div>
+            <button type="button" class="nobtn" onClick={() => this.makeApiCall()}>✗</button>
+        </div>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -20,7 +45,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  )}
 }
 
 export default App;
