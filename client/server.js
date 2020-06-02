2da635e6be4c23edc3780767bc06f14ef6549ea9
Peter <pc.dean@icloud.com>
2020-06-02 12:36:57 +0100

    add env variables for api keys

--- client/server.js
+++ client/server.js
@@ -18,12 +18,12 @@ app.listen(process.env.PORT || 8080);
 var Twitter = require('twitter');
 
 var client = new Twitter({
-  consumer_key: 
-  consumer_secret:
-  bearer_token: 
+  consumer_key: process.env.REACT_API_Key,
+  consumer_secret: process.env.REACT_APP_API_SECRET_KEY,
+  bearer_token: process.env.REACT_APP_TWITTER_BEARER_TOKEN
 })
 
-function makeApiCall() { 
+function makeApiCall() {
   client.get('search/tweets', {q: 'node.js'}, function(error, tweets, response) {
     console.log(tweets);
  });
