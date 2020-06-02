a9aeb2c57f46a6ea5cd7db5e4f45e5a3e38b65f3
James Oddy <jamesoddy8@gmail.com>
2020-06-02 17:09:35 +0100

    added mapprovider/mappify

--- client/src/App.js
+++ client/src/App.js
@@ -11,23 +11,31 @@ import './App.css';
 import 'ol/ol.css';
 import 'antd/dist/antd.css';
 import './react-geo.css';
+import {fromLonLat} from 'ol/proj';
 
 import {
   SimpleButton,
   MapComponent,
-  NominatimSearch
+  NominatimSearch,
+  MapProvider,
+  mappify
 } from '@terrestris/react-geo';
+
+const MappifiedNominatimSearch = mappify(NominatimSearch);
+const Map = mappify(MapComponent);
+
 
 const layer = new OlLayerTile({
   source: new OlSourceOsm()
 });
-
-const center = [ 788453.4890155146, 6573085.729161344 ];
+const london = [ -0.126286, 51.508623 ];
+const londonWebMercator = fromLonLat(london);
+// const center = [ 788453.4890155146, 6573085.729161344 ];
 
 const map = new OlMap({
   view: new OlView({
-    center: center,
-    zoom: 16,
+    center: londonWebMercator,
+    zoom: 15,
   }),
   layers: [layer]
 });
@@ -40,26 +48,29 @@ function App() {
 }
   return (
     <div className="App">
-        <MapComponent
-          map={map}
-        />
-      <SimpleButton
-         style={{position: 'fixed', top: '30px', right: '30px'}}
-         onClick={toggleDrawer}
-         icon="bars"
-       />
-       <Drawer
-         title="react-geo-application"
-         placement="right"
-         onClose={toggleDrawer}
-         visible={visible}
-         mask={false}
-       >
-        <NominatimSearch
+      <MapProvider map={map}>
+     <Map />
+     <SimpleButton
+       style={{position: 'fixed', top: '30px', right: '30px'}}
+       onClick={toggleDrawer}
+       icon="bars"
+     />
+     <Drawer
+       title="isitbusyornot?"
+       placement="right"
+       onClose={toggleDrawer}
+       visible={visible}
+       mask={false}
+     >
+       <MappifiedNominatimSearch
+         countrycodes="gb"
+         placeholder="isitbusyornot...?"
          key="search"
          map={map}
+         format="json"
        />
-       </Drawer>
+        </Drawer>
+      </MapProvider>
     </div>
   );
 }
