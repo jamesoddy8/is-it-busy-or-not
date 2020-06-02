1f1106202c5805e6e92a82ed370f7d4164762f08
James Oddy <jamesoddy8@gmail.com>
2020-06-02 14:34:08 +0100

    added nominatim

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import OlMap from 'ol/Map';
 import OlView from 'ol/View';
 import OlLayerTile from 'ol/layer/Tile';
 import OlSourceOsm from 'ol/source/OSM';
+
+import { Drawer } from 'antd';
 
 import './App.css';
 import 'ol/ol.css';
@@ -11,14 +13,16 @@ import 'antd/dist/antd.css';
 import './react-geo.css';
 
 import {
-  MapComponent
+  SimpleButton,
+  MapComponent,
+  NominatimSearch
 } from '@terrestris/react-geo';
 
 const layer = new OlLayerTile({
   source: new OlSourceOsm()
 });
 
-const center = [ 51282, 1654 ];
+const center = [ 788453.4890155146, 6573085.729161344 ];
 
 const map = new OlMap({
   view: new OlView({
@@ -29,12 +33,33 @@ const map = new OlMap({
 });
 
 function App() {
+  const [visible, setVisible] = useState(false);
+
+  const toggleDrawer = () => {
+  setVisible(!visible);
+}
   return (
     <div className="App">
         <MapComponent
           map={map}
         />
-
+      <SimpleButton
+         style={{position: 'fixed', top: '30px', right: '30px'}}
+         onClick={toggleDrawer}
+         icon="bars"
+       />
+       <Drawer
+         title="react-geo-application"
+         placement="right"
+         onClose={toggleDrawer}
+         visible={visible}
+         mask={false}
+       >
+        <NominatimSearch
+         key="search"
+         map={map}
+       />
+       </Drawer>
     </div>
   );
 }
