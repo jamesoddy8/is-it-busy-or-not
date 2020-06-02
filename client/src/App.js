947b2d5e9ebeb2400ebc0ef66bfb77c004dd3376
James Oddy <jamesoddy8@gmail.com>
2020-06-02 18:20:10 +0100

    added comments

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,5 @@
+ // docs: https://terrestris.github.io/react-geo-ws/map-integration/nominatim-search.html
+
 import React, { useState } from 'react';
 
 import OlMap from 'ol/Map';
@@ -7,35 +9,28 @@ import OlSourceOsm from 'ol/source/OSM';
 
 import { Drawer } from 'antd';
 
+import {
+  SimpleButton,
+  MapComponent,
+  NominatimSearch,
+} from '@terrestris/react-geo';
+
 import './App.css';
 import 'ol/ol.css';
 import 'antd/dist/antd.css';
 import './react-geo.css';
 import {fromLonLat} from 'ol/proj';
 
-import {
-  SimpleButton,
-  MapComponent,
-  NominatimSearch,
-  MapProvider,
-  mappify
-} from '@terrestris/react-geo';
-
-const MappifiedNominatimSearch = mappify(NominatimSearch);
-const Map = mappify(MapComponent);
-
-
 const layer = new OlLayerTile({
   source: new OlSourceOsm()
 });
 const london = [ -0.126286, 51.508623 ];
 const londonWebMercator = fromLonLat(london);
-// const center = [ 788453.4890155146, 6573085.729161344 ];
 
 const map = new OlMap({
   view: new OlView({
     center: londonWebMercator,
-    zoom: 15,
+    zoom: 14,
   }),
   layers: [layer]
 });
@@ -48,8 +43,9 @@ function App() {
 }
   return (
     <div className="App">
-      <MapProvider map={map}>
-     <Map />
+      <MapComponent
+        map={map}
+        />
      <SimpleButton
        style={{position: 'fixed', top: '30px', right: '30px'}}
        onClick={toggleDrawer}
@@ -62,15 +58,16 @@ function App() {
        visible={visible}
        mask={false}
      >
-       <MappifiedNominatimSearch
+     {/*need to get search value from input & send to twitter api - source code: https://github.com/terrestris/react-geo/blob/master/src/Field/NominatimSearch/NominatimSearch.tsx  - somehow pass "searchTerm" state on line 178 (inside link) and pass it on*/}
+
+       <NominatimSearch
          countrycodes="gb"
          placeholder="isitbusyornot...?"
          key="search"
          map={map}
          format="json"
        />
-        </Drawer>
-      </MapProvider>
+      </Drawer>
     </div>
   );
 }
