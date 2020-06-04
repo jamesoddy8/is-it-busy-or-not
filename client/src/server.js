4ce1f797a5333115cdd33e011d1434bde2fabb28
James Oddy <jamesoddy8@gmail.com>
2020-06-05 00:40:41 +0100

    changed server.js inside client

--- client/src/server.js
+++ client/src/server.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(path.join(__dirname, 'build')));
+
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'build')));
+  // Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  });
+}
 
 const bodyParser = require('body-parser')
 app.use(bodyParser.json());
